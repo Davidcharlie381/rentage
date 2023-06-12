e291e2746037eade23c8ffdcf2749f66b420cb13
davidcharlie381 <davidcharleswisdom@gmail.com>
2023-06-12 15:37:13 +0100

    start

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { Montserrat } from 'next/font/google'
 
 const mont = Montserrat({ subsets: ['latin'] })
@@ -6,9 +5,9 @@ const mont = Montserrat({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 font-extrabold ${mont.className}`}
     >
-
+      Rentage!!
     </main>
   )
 }
