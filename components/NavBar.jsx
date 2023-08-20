ba0f0e1ef101e8ef1a537f23d20dfc94249fdf7b
davidcharlie381 <davidcharleswisdom@gmail.com>
2023-08-20 01:00:21 +0100

    add login link to mobile navBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import { CgClose } from "react-icons/cg";
 import NavLink from "./NavLink";
+import Link from "next/link";
 
-const NavBar = ({handleClose, open}) => {
-
-
+const NavBar = ({ handleClose, open }) => {
   return (
-    <section className={`md:hidden ${open ? "left-0" : "-left-[100vh]"} fixed z-[999] transiton-all duration-500 bg-lightBlue opacity-[0.97] w-screen h-screen inset-0`}>
+    <section
+      className={`md:hidden ${
+        open ? "left-0" : "-left-[100vh]"
+      } fixed z-[999] transiton-all duration-500 bg-lightBlue opacity-[0.97] w-screen h-screen inset-0`}
+    >
       <div className="container flex flex-col gap-10 pt-5">
         <div className="text-3xl flex flex-row-reverse" onClick={handleClose}>
           <CgClose />
         </div>
-        <div className="text-center text-[21px] font-bold hover:text-orange">
+        <Link
+          href="/login-register"
+          className="text-center text-[21px] font-bold hover:text-orange"
+        >
           Sign in
-        </div>
+        </Link>
         <div className="font-bold">
           <ul className="flex gap-6 flex-col pl-2">
-            <NavLink path="/" name="Home" nav handleClose={handleClose}/>
-            <NavLink path="/properties" name="Browse properties" nav handleClose={handleClose}/>
-            <NavLink path="/add-property" name="Add your own listing" nav handleClose={handleClose}/>
+            <NavLink path="/" name="Home" nav handleClose={handleClose} />
+            <NavLink
+              path="/properties"
+              name="Browse properties"
+              nav
+              handleClose={handleClose}
+            />
+            <NavLink
+              path="/add-property"
+              name="Add your own listing"
+              nav
+              handleClose={handleClose}
+            />
           </ul>
         </div>
       </div>
